ce4b8f9e612e876c2f06057116eaadd35372dcf7
Matheus Rian <souzamatheusrian@gmail.com>
2021-03-02 22:11:39 -0300

    feat: type-any ✨

---
+++ src/tiposBasicos/type-any.ts
@@ -0,0 +1,9 @@
+// Any - Qualquer coisa/Sem Tipagem
+// Utilize any em ultimo caso
+function showMessage(msg: any) {
+  return msg;
+}
+
+console.log(showMessage('Óla'));
+console.log(showMessage(1));
+console.log(showMessage([1, 2, 3]));
