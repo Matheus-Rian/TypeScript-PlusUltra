426f7eeb931020f0c265fd73f42ee00673502c4f
Matheus Rian <souzamatheusrian@gmail.com>
2021-03-03 22:08:13 -0300

    feat: type union-type ✨

---
+++ src/tiposBasicos/type-union-type.ts
@@ -0,0 +1,10 @@
+// Union-types - |(ou)
+// string | number | boolean - A var pode ser desses três tipos
+
+function addOrConcat(a: number | string, b: number | string): number | string {
+  if (typeof a === 'number' && typeof b === 'number') return a + b;
+  return `${a}${b}`;
+}
+
+console.log(addOrConcat(2, 3));
+console.log(addOrConcat('2', '3'));
