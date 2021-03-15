0c2096d0a858d6637169800c1515f17062c6cf79
Matheus Rian <souzamatheusrian@gmail.com>
2021-03-14 22:32:56 -0300

    feat: classes ✨

--- src/classes/01-classes.ts
+++ src/classes/01-classes.ts
@@ -9,6 +9,14 @@ export class Empresa {
     this.nome = nome;
     this.cnpj = cnpj;
   }
+
+  adicionarColaborador(colaborador: Colaborador): void {
+    this.colaboradores.push(colaborador);
+  }
+
+  mostrarColaboradores(): void {
+    for (const colaborador of this.colaboradores) [console.log(colaborador)];
+  }
 }
 
 // Forma menor - Isso faz tudo da forma maior
@@ -17,14 +25,15 @@ export class Colaborador {
   constructor(
     public readonly nome: string,
     public readonly sobrenome: string,
-  ) {
-
-  }
+  ) {}
 }
 
 const empresa1 = new Empresa('Matheus', '121221');
 const colaborador1 = new Colaborador('Rian', 'Souza');
 const colaborador2 = new Colaborador('Silva', 'Matheus');
 const colaborador3 = new Colaborador('Souza', 'Rian');
-
+empresa1.adicionarColaborador(colaborador1);
+empresa1.adicionarColaborador(colaborador2);
+empresa1.adicionarColaborador(colaborador3);
+empresa1.mostrarColaboradores();
 console.log(empresa1);
