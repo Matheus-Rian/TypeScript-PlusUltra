d601e133cb5db50deb3f4a19b6f3692271ad3e7c
Matheus Rian <souzamatheusrian@gmail.com>
2021-04-10 08:55:52 -0300

    feat: keys types ✨

---
+++ src/tiposAvancados/03-chaves-em-tipos.ts
@@ -0,0 +1,18 @@
+type Veiculo = {
+  marca: string;
+  ano: string;
+};
+
+type Car = {
+  brand: Veiculo['marca']; // Pegando a tipagem da chave marca do type Veiculo
+  year: Veiculo['ano'];
+  name: string;
+};
+
+const carro: Car = {
+  brand: 'ford',
+  year: '2020',
+  name: 'Nome',
+};
+
+console.log(carro);
