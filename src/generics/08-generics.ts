40d138c4bd6740c4dd25f656d4e4d90e30ea0401
Matheus Rian <souzamatheusrian@gmail.com>
2021-05-04 17:18:26 -0300

    feat: Utility Types ✨

--- src/generics/08-generics.ts
+++ src/generics/08-generics.ts
@@ -1,3 +1,5 @@
+// Utility Types - Generics padrão no TS
+
 // Record
 const objeto1: Record<string, string | number> = {
   nome: 'Matheus',
@@ -15,6 +17,12 @@ type PessoaProtocol = {
 
 // Required
 type PessoaRequired = Required<PessoaProtocol>;
+// Partial
+type PessoaPartial = Partial<PessoaRequired>;
+// Readonly
+type PessoaReadonly = Readonly<PessoaRequired>;
+// Pick
+type PessoaPick = Pick<PessoaRequired, 'idade' | 'nome'>;
 
 const objeto2: PessoaRequired = {
   nome: 'Matheus',
@@ -24,5 +32,36 @@ const objeto2: PessoaRequired = {
 
 console.log(objeto2);
 
+// Extract e Exclude
+type ABC = 'A' | 'B' | 'C';
+type CDE = 'C' | 'D' | 'E';
+type tipoExclude = Exclude<ABC, CDE>;
+type tipoExtract = Extract<ABC, CDE>;
+
+type AccountMongo = {
+  _id: string;
+  nome: string;
+  idade: number;
+  sobrenome: string;
+};
+
+type AccountApi = Pick<AccountMongo, Exclude<keyof AccountMongo, '_id'>> & {
+  id: string;
+};
+
+const accountMongo: AccountMongo = {
+  _id: 'ddqwdsadasdwqdw22',
+  nome: 'Matheus',
+  idade: 18,
+  sobrenome: 'Rian',
+};
+
+function mapAccount(accountMongo: AccountMongo): AccountApi {
+  const { _id, ...accountData } = accountMongo;
+  return { ...accountData, id: _id };
+}
+
+const accountApi = mapAccount(accountMongo);
+console.log(accountApi);
 // Module Mode
 export default 1;
