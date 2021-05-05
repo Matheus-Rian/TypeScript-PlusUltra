f0fbbe67d3aaad3f0f944a6487d297ef124818bf
Matheus Rian <souzamatheusrian@gmail.com>
2021-05-05 15:13:44 -0300

    feat: solution exercise ✨

--- src/exercicios/votacao.ts
+++ src/exercicios/votacao.ts
@@ -4,12 +4,8 @@ interface candidate {
   qtndVotes: number;
 }
 
-abstract class Voting {
+class Voting {
   constructor(public names: candidate[]) {}
-  abstract processVoting(vote: string): void;
-}
-
-class Linguagens extends Voting {
   processVoting(vote: string): void {
     this.names.forEach((candidate) => {
       if (candidate.name === vote) candidate.qtndVotes++;
@@ -31,6 +27,70 @@ const ling: candidate[] = [
     qtndVotes: 0,
   },
 ];
-const matheus = new Linguagens(ling);
-matheus.processVoting('Java');
-console.log(matheus);
+const votation = new Voting(ling);
+votation.processVoting('Java');
+
+// Solução do Professor
+
+type VotationOption = {
+  numberOfVotes: number;
+  option: string;
+};
+
+export class Votation {
+  private _votationOptions: VotationOption[] = [];
+  constructor(public details: string) {}
+
+  get votationOptions(): VotationOption[] {
+    return this._votationOptions;
+  }
+
+  addVotationOption(votationOption: VotationOption): void {
+    this._votationOptions.push(votationOption);
+  }
+
+  vote(votationIndex: number): void {
+    if (!this._votationOptions[votationIndex]) return;
+    this._votationOptions[votationIndex].numberOfVotes += 1;
+  }
+}
+
+export class VotationApp {
+  private votations: Votation[] = [];
+
+  addVotation(votation: Votation): void {
+    this.votations.push(votation);
+  }
+
+  showVotations(): void {
+    for (const votation of this.votations) {
+      console.log(votation.details);
+      for (const votationOption of votation.votationOptions) {
+        console.log(votationOption.option, votationOption.numberOfVotes);
+      }
+      console.log('###');
+      console.log('');
+    }
+  }
+}
+
+const votation1 = new Votation('Qual a sua linguagem de programação favorita?');
+votation1.addVotationOption({ option: 'Python', numberOfVotes: 0 });
+votation1.addVotationOption({ option: 'JavaScript', numberOfVotes: 0 });
+votation1.addVotationOption({ option: 'TypeScript', numberOfVotes: 0 });
+votation1.vote(0);
+votation1.vote(0);
+
+const votation2 = new Votation('Qual a sua cor favorita?');
+votation2.addVotationOption({ option: 'Azul', numberOfVotes: 0 });
+votation2.addVotationOption({ option: 'Verde', numberOfVotes: 0 });
+votation2.addVotationOption({ option: 'Amarelo', numberOfVotes: 0 });
+votation2.vote(0);
+votation2.vote(0);
+
+const votationApp = new VotationApp();
+votationApp.addVotation(votation1);
+votationApp.addVotation(votation2);
+votationApp.showVotations();
+
+export default 1;
