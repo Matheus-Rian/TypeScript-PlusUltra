88299ca6e5b09aeb61da12d04757bee0630febaf
Matheus Rian <souzamatheusrian@gmail.com>
2022-08-04 10:16:48 -0300

    feat: add super

--- src/ts/pillarsPOO/heranca.ts
+++ src/ts/pillarsPOO/heranca.ts
@@ -15,8 +15,18 @@ export class Person {
 }
 
 export class Student extends Person {
+  constructor(
+    name: string,
+    age: number,
+    cpf: string,
+    public classRoom: string,
+  ) {
+    super(name, age, cpf);
+  }
+
   getCpf(): string {
-    return 'this is Student ' + this.cpf;
+    console.log('Fazendo algo antes');
+    return super.getCpf();
   }
 }
 
@@ -26,8 +36,9 @@ export class Client extends Person {
   }
 }
 
-const student = new Student('Matheus', 19, '111.111.111-11');
+const student = new Student('Matheus', 19, '111.111.111-11', '7B');
 const person = new Person('Rian', 19, '111.111.111-11');
 const client = new Client('Souza', 19, '111.111.111-11');
 
 console.log(student.getCpf(), person, client);
+console.log(student);
